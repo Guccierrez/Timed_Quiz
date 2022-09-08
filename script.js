ba2fd65250e138de401a35aec3971a2a407fbb42
Fabian Gutierrez <fabiangutierrez580@gmail.com>
2022-09-07 19:05:13 -0500

    Revert "Both questions AND answer choices pop up"
    This reverts commit 8c725f8527ba46092716d730cc2e82670e2df818.

--- script.js
+++ script.js
@@ -102,6 +102,12 @@ function displayMessage(){
 }
 
 
+// console.log(currentQuestionIndex)
+// buttonsDiv.textContent=questions[0].question;
+// A.textContent = questions[0].choices[0];
+// B.textContent = questions[0].choices[1];
+// C.textContent = questions[0].choices[2];
+// D.textContent = questions[0].choices[3];
 
 
 
@@ -115,14 +121,14 @@ function displayMessage(){
 
 
 
-/* GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and my score*/
+// /* GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score*/
